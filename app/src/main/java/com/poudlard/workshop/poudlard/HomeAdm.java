4f0f6f6f5d031abe4bf26b93fac9ef77568ead28
Malmass <sebmalmasson@gmail.com>
2018-04-12 09:42:35 +0200

    architecture_final

---
+++ app/src/main/java/com/poudlard/workshop/poudlard/HomeAdm.java
@@ -0,0 +1,12 @@
+package com.poudlard.workshop.poudlard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeAdm extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin);
+    }
+}
